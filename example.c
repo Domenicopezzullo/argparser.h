43d76d2691295ac0ad1cfa31df316a623eb76da6
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-04-18 20:43:58 +0200

    yahh

--- example.c
+++ example.c
@@ -3,7 +3,7 @@
 #define PARSER_IMPLEMENTATION
 #include "argparser.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   Parser parser = parser_new();
   add_flag(&parser, "argc", "argv");
   ParseResult *results = parser_parse(&parser, argc, argv);
